7e935ba257e63b51add598768d7ff54758a749c3
50an6xy06r6n <garethenator@gmail.com>
2021-02-23 02:06:13 -0800

    Test using EEPROM to persist lighting settings (success!)

---
+++ keyboards/massdrop/ctrl/keymaps/eeprom/rules.mk
@@ -0,0 +1,7 @@
+# This keymap requires Massdrop Configurator support
+# VIA_ENABLE = yes
+# LTO_ENABLE = yes
+# BOOTMAGIC_ENABLE = full
+# RAW_ENABLE = yes
+CONSOLE_ENABLE = yes          	# Console for debug
+OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
