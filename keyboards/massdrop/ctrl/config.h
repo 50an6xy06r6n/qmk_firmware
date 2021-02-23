8b4461f44ea8d532401bc3d6f5daca40035a075c
50an6xy06r6n <garethenator@gmail.com>
2021-02-23 03:06:09 -0800

    Common changes

--- keyboards/massdrop/ctrl/config.h
+++ keyboards/massdrop/ctrl/config.h
@@ -94,6 +94,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+#define IS_COMMAND() (get_mods() == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 //#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
